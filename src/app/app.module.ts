757e26f0a7d5570f3dc88efa10523da61beef82a
rohhan <rohhan@gmail.com>
2019-08-17 18:05:41 -0700

    Added material.io

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GuildsComponent } from './guilds/guilds.component';
@@ -11,8 +13,9 @@ import { GuildsComponent } from './guilds/guilds.component';
     GuildsComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
