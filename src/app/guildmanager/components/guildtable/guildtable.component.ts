f6165dca36b8efc25fad2411e28c584fcc27de67
rohhan <rohhan@gmail.com>
2019-09-06 19:51:38 -0700

    Now pulling in real data to guild table

--- src/app/guildmanager/components/guildtable/guildtable.component.ts
+++ src/app/guildmanager/components/guildtable/guildtable.component.ts
@@ -1,31 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { IGuild } from 'src/app/models/guild';
+import { GuildService } from 'src/app/services/guild.service';
 
 @Component({
   selector: 'app-guild-table',
   templateUrl: './guildtable.component.html',
   styleUrls: ['./guildtable.component.scss']
 })
-export class GuildTableComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+export class GuildTableComponent implements OnInit {
+  displayedColumns: string[] = ['dateCreated', 'guildId', 'guildName', 'ownerId'];
+  dataSource: MatTableDataSource<IGuild>;
+  guildz: IGuild[];
+
+  constructor(private guildService: GuildService) { }
+
+  ngOnInit() {
+    this.guildService.GetGuilds().subscribe({
+      next: guilds => {
+        this.dataSource = new MatTableDataSource<IGuild>(guilds);
+        this.guildz = guilds;
+        console.log(this.guildz);
+      }
+    });
+  }
 }
